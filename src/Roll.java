8131b181c8bfe2b98c7da2e785a0ba0239b9495b
Paul Miller <dumpsteritis@gmail.com>
2016-03-08 14:21:47 -0600

    Rules Class constructed to return value of rolled dice

--- src/Roll.java
+++ src/Roll.java
@@ -1,4 +1,47 @@
+import java.util.Random;
 
-public class Roll {
-
+public class Roll extends Game
+{	
+	public int dicefour()
+	{
+		int number;	
+		Random dicefour = new Random();
+		number = 1+dicefour.nextInt(4);			
+		return number;	
+	}
+	public int dicesix()
+	{
+		int number;	
+		Random dicesix = new Random();
+		number = 1+dicesix.nextInt(6);			
+		return number;	
+	}
+	public int diceeight()
+	{
+		int number;	
+		Random diceeight = new Random();
+		number = 1+diceeight.nextInt(8);			
+		return number;	
+	}
+	public int diceten()
+	{
+		int number;	
+		Random diceten = new Random();
+		number = 1+diceten.nextInt(10);			
+		return number;	
+	}
+	public int dicetwelve()
+	{
+		int number;	
+		Random dicetwelve = new Random();
+		number = 1+dicetwelve.nextInt(12);			
+		return number;	
+	}
+	public int dicetwenty()
+	{
+		int number;	
+		Random dicetwenty = new Random();
+		number = 1+dicetwenty.nextInt(20);			
+		return number;	
+	}	
 }
